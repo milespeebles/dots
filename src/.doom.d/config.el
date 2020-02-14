91d3aee74c193fed614512ccc2be85be80ef84c9
anonymous <>
2020-02-14 09:28:49 -0600

    add files

--- src/.doom.d/config.el
+++ src/.doom.d/config.el
@@ -1,56 +1,7 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+(setq doom-theme 'doom-outrun-electric)
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; refresh' after modifying this file!
+(setq doom-font (font-spec :family "Gintronic" :size 12 :weight 'bold))
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-one)
-
-;; If you intend to use org, it is recommended you change this!
-(setq org-directory "~/org/")
-
-;; If you want to change the style of line numbers, change this to `relative' or
-;; `nil' to disable it:
-(setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', where Emacs
-;;   looks when you load packages with `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; they are implemented.
-
-;; ==============
-;;
 (setq user-full-name "anonymous"
       user-mail-address "")
 
@@ -68,7 +19,7 @@
   (lambda ()
     (use-package org-drill)))
 
-(setq org-bullets-bullet-list '("◉"))
+(setq org-bullets-bullet-list '("✿"))
 
 (setq org-refile-targets '((nil :maxlevel . 9)
                                 (org-agenda-files :maxlevel . 9)))
@@ -76,6 +27,8 @@
 (setq org-outline-path-complete-in-steps nil)
 
 (setq org-refile-use-outline-path t)
+
+(setq org-agenda-skip-deadline-prewarning-if-scheduled t)
 
 (setq company-dabbrev-downcase nil)
 
@@ -100,10 +53,56 @@
           web-mode-enable-auto-pairing t
           web-mode-auto-close-style 2)))
 
-(custom-set-faces
-  '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
-  '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
-  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
-  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
-  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
-)
+;; (setq user-full-name "anonymous"
+;;       user-mail-address "")
+;;
+;; (setq doom-font (font-spec :family "Gintronic" :size 14 :weight 'bold))
+;;
+;; (setq display-line-numbers-type `visual)
+;;
+;; (unless window-system
+;;   (xterm-mouse-mode)
+;;   (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+;;   (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
+;;
+;; (add-hook 'org-mode-hook #'auto-fill-mode)
+;;
+;; (add-hook 'org-mode-hook
+;;   (lambda ()
+;;     (use-package org-drill)
+;;     (custom-set-faces
+;;       '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+;;       '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
+;;       '(org-level-3 ((t (:inherit outline-2 :height 1.1))))
+;;       '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+;;       '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))))
+;;
+;; (setq org-agenda-skip-deadline-prewarning-if-scheduled nil
+;;       org-bullet-bullets-list '("✿")
+;;       org-refile-targets '((nil :maxlevel . 9)
+;;         (org-agenda-files :maxlevel . 9))
+;;       org-outline-path-complete-in-steps nil
+;;       org-refile-use-outline-path t
+;;       org-image-actual-width nil
+;;       org-directory "~/sync/org"
+;;       org-ellipsis " ▼ ")
+;;
+;;
+;; (add-hook 'company-mode-hook
+;;   (lambda ()
+;;     (setq company-dabbrev-downcase nil
+;;           company-idle-delay 0.2)))
+;;
+;; (remove-hook 'company-mode-hook #'company-prescient-mode)
+;;
+;; (add-hook 'web-mode-hook
+;;   (lambda ()
+;;     (setq web-mode-script-padding 0
+;;           web-mode-style-padding 0
+;;           web-mode-block-padding 2
+;;           web-mode-markup-indent-offset 2
+;;           web-mode-css-indent-offset 2
+;;           web-mode-code-indent-offset 2
+;;           web-mode-enable-auto-closing t
+;;           web-mode-enable-auto-pairing t
+;;           web-mode-auto-close-style 2)))
